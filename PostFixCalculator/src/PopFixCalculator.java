fe2c60433488e5ecbd1a4b32707ded687cd9ab3e
gundarsv <267009@viauc.dk>
2019-03-19 13:53:09 +0100

    Added calculator

---
+++ PostFixCalculator/src/PopFixCalculator.java
@@ -0,0 +1,27 @@
+public class PopFixCalculator {
+    public double CalculatingResult(String elements)
+    {
+        Stack stackOfElements = new Stack(100);
+        char lastPeeked;
+        for (int i = 0; i <elements.length(); i++) {
+            stackOfElements.push(elements.charAt(i));
+        }
+        Stack stackOfNumbers = new Stack(100);
+        for (int i = 0; i < stackOfElements.size(); i++) {
+            lastPeeked = (Character) stackOfElements.peek();
+            if (Character.isDigit(lastPeeked)) {
+                stackOfNumbers.push(stackOfElements.pop());
+
+            }
+            if (lastPeeked == '%' || lastPeeked == '/' || lastPeeked == '*' || lastPeeked == '+' || lastPeeked == '-') {
+                
+
+            }
+        }
+            return 2;
+    }
+
+
+    }
+
+
